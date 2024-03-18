02e3cf37549c4a3c01513ddd98d7f98da2367f0d
lexeach <80155340+lexeach@users.noreply.github.com>
2024-03-18 17:10:50 +0530

    Update home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -319,8 +319,8 @@ const Dashboard = () => {
     event.preventDefault();
     try {
       const isEthereumAddress = /^(0x)?[0-9a-fA-F]{40}$/.test(referrerId);
-      let all = Number(pool1_price / 10 ** 12) + Number(registration_Free / 10 ** 12);
-      let total = Number(all) + Number((all * taxRate) / 100);
+      let all = Number(pool1_price) + Number(registration_Free);
+      let total = (Number(all) + Number((all * taxRate) / 100)) / Number(10 **12);
       let amount = web3.utils.toWei(total.toString(), "ether") ;
       let FPrint_ = new web3.eth.Contract(FPrint.ABI, FPrint.address);
       let USDT_ = new web3.eth.Contract(USDT.ABI, USDT.address);
